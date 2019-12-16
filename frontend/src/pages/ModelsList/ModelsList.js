dd8572e5bfac948955406e9b34bbab12b68e6ae8
teonaandreea <teona.stoica@tradeshift.com>
2019-12-16 12:53:15 +0100

    add pagination & server-side search

--- frontend/src/pages/ModelsList/ModelsList.js
+++ frontend/src/pages/ModelsList/ModelsList.js
@@ -10,16 +10,62 @@ class ModelsList extends Component {
         super();
         this.state = {
             models: [],
+            loading: false,
+            page: 0,
+            totalPages: 1
         };
+        this.timeout = null;
     }
 
     componentDidMount() {
-        fetch('/iceberg/multi', {
+        fetch(`/iceberg/multi/?page=${this.state.page}`, {
             accept: 'application/json',
         })
             .then(results => results.json())
-            .then(data => this.setState({ models: data }))
+            .then(data => {
+                this.setState({ models: data, totalPages: 30 })
+            })
             .catch(err => console.log(err));
+    }
+
+    fetchData(state) {
+        this.setState({ loading: true });
+        const baseUrl = `/iceberg/multi/?page=${state.page}`;
+
+        if(state.filtered.length === 0) {
+            this.setState({ page: state.page });
+
+            // fetch data for that page
+            fetch(baseUrl, {
+                accept: 'application/json',
+            })
+                .then(results => results.json())
+                .then(data => {
+                    this.setState({ models: data, loading: false })
+                })
+                .catch(err => console.log(err));
+        } else if (state.filtered.length > 0) {
+            this.setState({ page: 0 });
+            let searchUrl = baseUrl;
+
+            state.filtered.forEach(filter => {
+                searchUrl += `&${filter.id}=${filter.value}`;
+            });
+
+            if(this.timeout) {clearTimeout(this.timeout)}
+            this.timeout = setTimeout(() => {
+                // perform search
+                fetch(searchUrl, {
+                    accept: 'application/json',
+                })
+                    .then(results => results.json())
+                    .then(data => {
+                        console.log('data', data);
+                        this.setState({ models: data, loading: false })
+                    })
+                    .catch(err => console.log(err));
+            }, 500);
+        }
     }
 
     render() {
@@ -65,7 +111,11 @@ class ModelsList extends Component {
                         className="highlight"
                         defaultPageSize={15}
                         pageSizeOptions={[5, 10, 15, 20, 25, 50, 100]}
+                        pages={this.state.totalPages}
+                        loading={this.state.loading}
                         filterable
+                        manual // informs React Table that you'll be handling sorting and pagination server-side
+                        onFetchData={(state) => this.fetchData(state)}
                     />
                 </ModelsListStyles>
             </>
